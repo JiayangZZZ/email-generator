ec387f11c10bb9e3eb106987af0d722b609cf57e
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-05-08 14:09:58 +0800

    added footer

---
+++ components/footerInfo.js
@@ -0,0 +1,9 @@
+
+var footerInfo = [{
+  copyright : 'Copyright © 2014 Tantan, All rights reserved.',
+  emailName : 'info@tantanapp.com',
+  emailAddress : 'info@tantanapp.com'
+}];
+
+module.exports = footerInfo;
+
