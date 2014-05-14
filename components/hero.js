5b0bceb152f4d696e6490eb826a62929323cc782
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-05-14 17:18:05 +0800

    added hero background url

--- components/hero.js
+++ components/hero.js
@@ -6,7 +6,7 @@ module.exports = function(locale) {
   return [{
     title : gt('HERO_TITLE'),
     slogen : gt('HERO_SLOGEN'),
-    background : '',
+    background : 'http://static.p1.com/email/images/tantan-email-header.png',
     backgroundColor : '#f5f6f6'
   }];
 };
